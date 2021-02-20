d71702e7b78577fc6a3762bba5d829c2f80f4d42
Ranieri Valença <ranierivalenca@gmail.com>
2021-02-20 14:44:35 -0300

    Improving layout using Tailwindcss and creating a Checkbox Svelte Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
   import Tailwindcss from './Tailwind.svelte'
+  import Checkbox from './Checkbox.svelte'
+
+
 
   const HOUR = 60 * 60 * 1000
   const DAY = 24 * HOUR
@@ -18,7 +21,7 @@
   const HOLIDAYS = ['21/04', '01/05', '03/06', '07/09', '11/10', '12/10', '28/10', '01/11', '02/11', '15/11', '08/12', '24/12', '25/12', '31/12', '01/01', '01/02', '02/02']
 
   let holidays = {
-    '03/06': 'Corpus Christi',
+    '03/06/2021': 'Corpus Christi',
     '29/09': 'Feriado local',
     '11/10': 'Nossa Senhora Aparecida',
     '28/10': 'Dia do Servidor Público',
@@ -28,7 +31,21 @@
     '02/02/2022': 'Carnaval'
   }
 
-  let holidays_dates = Object.keys(holidays)
+  let fixed_holidays_dates = []
+  let moving_holidays_dates = []
+  let setHolidaysDates = () => {
+    fixed_holidays_dates = [...Object.keys(holidays), ...Object.keys(NATIONAL_HOLIDAYS)]
+      .filter(date => date.length == 5)
+      .map(date => date.split('/').reverse().join('-'))
+      .sort()
+      .map(date => date.split('-').reverse().join('/'))
+    moving_holidays_dates = [...Object.keys(holidays), ...Object.keys(NATIONAL_HOLIDAYS)]
+      .filter(date => date.length == 10)
+      .map(date => date.split('/').reverse().join('-'))
+      .sort()
+      .map(date => date.split('-').reverse().join('/'))
+  }
+  setHolidaysDates()
 
 
   let start_date = '2021-10-22'
@@ -45,10 +62,12 @@
   let formatted_days = []
   let formatted_holidays = []
 
+  let show_holidays = false
+
 
   let removeHoliday = key => {
     delete holidays[key]
-    holidays_dates = Object.keys(holidays)
+    setHolidaysDates()
     console.log(holidays)
   }
 
@@ -164,139 +183,160 @@
 
 //    days_per_week++
 //    days_per_week--
-    console.log(days)
-    console.log(formatted_days)
-    console.log(formatDate(end_timestamp))
-    console.log(formatted_holidays)
+    // console.log(days)
+    // console.log(formatted_days)
+    // console.log(formatDate(end_timestamp))
+    // console.log(formatted_holidays)
 
-  })(days_per_week, use_saturdays, start_date, holidays_dates)
+  })(days_per_week, use_saturdays, start_date, fixed_holidays_dates)
   //  $: (() => setup(dias_por_semana))(usar_sabados)
 
 
 </script>
 
-<style>
-  * {
-    font-family: sans-serif;
-  }
-  table {
-    margin-top: 1em;
-  }
-  table, td, th {
-    border: 1px solid black;
-    border-collapse: collapse
-  }
-  td, th {
-    padding: .5em .8em;
-    text-align: center;
-  }
-  td.dia {
-    background: hsl
-  }
-  tr:first-child > th {
-    border-bottom-width: 5px;
-  }
-  th:first-child, td:first-child {
-    border-right-width: 5px;
-  }
-  th:last-child, td:last-child {
-    /*background: hsl(0, 0%, 80%);*/
-    border-left-width: 5px;
-  }
-  td.special {
-    color: hsl(0, 100%, 30%)
-  }
-  button {
-    background: hsl(0, 0%, 100%);
-    padding: 0.5em 1em;
-  }
-  button:hover {
-    cursor: pointer;
-    background: hsl(120, 100%, 95%)
-  }
-  button.active {
-    background: hsl(120, 100%, 80%)
-  }
-  .disabled {
-    color: hsl(0, 0%, 80%)
-  }
-  .holiday {
-    margin: .2em;
-    background: hsl(200, 100%, 80%);
-    padding: .2em .5em;
-    border-radius: 1em;
-    display: inline-block;
-  }
-  .holiday > span {
-    border-radius: 50%;
-    background: hsl(20, 100%, 90%);
-    display: inline-block;
-    width: 1.2em;
-    height: 1.2em;
-    padding: 0;
-    text-align: center;
-    line-height: 1.2em;
-    cursor: pointer;
-  }
-  .national-holiday {
-    background: hsl(200, 100%, 30%);
-    color: white;
-  }
-</style>
-
 <Tailwindcss />
 
-<h1>Dias Letivos</h1>
+<div class="max-w-7xl bg-gray-100 m-auto min-h-screen">
+  <nav class="p-2 py-4 bg-green-600">
+    <h1 class="text-xl text-white">Dias Letivos</h1>
+  </nav>
 
-<input bind:value={start_date} type="date">
-<button class:active="{days_per_week == 5}" on:click={() => days_per_week = 5}>
-  Segunda a Sexta (5 dias letivos)
-</button>
-<button class:active="{days_per_week == 6}" on:click={() => days_per_week = 6}>
-  Segunda a Sábado (6 dias letivos)
-</button>
-<label class:disabled="{days_per_week != 5}">
-  <input type="checkbox" disabled="{days_per_week != 5}" bind:checked="{use_saturdays}" /> Usar sábados
-</label>
+  <main class="p-2">
 
-<h2>Feriados</h2>
-{#each holidays_dates as holiday}
-  <div class="holiday">
-    {holiday} - {holidays[holiday]} <span on:click={() => removeHoliday(holiday)}>&times;</span>
-  </div>
-{/each}
-{#each Object.keys(NATIONAL_HOLIDAYS) as holiday}
-  <div class="holiday national-holiday">
-    {holiday} - {NATIONAL_HOLIDAYS[holiday]}
-  </div>
-{/each}
+    <div class="border border-gray-300 rounded-lg shadow-lg p-2 grid grid-cols-3">
+      <div>
+        <label for="start_date" class="text-sm block">Dia Inicial:</label>
+        <input id="start_date" bind:value={start_date} type="date">
+      </div>
 
-<table>
-  <tr>
-    <th>Semana</th>
-    {#each range(number_of_weeks) as week}
-      <th>{week + 1}</th>
-    {/each}
-    <th>Feriados</th>
-  </tr>
-  {#each formatted_days as day, day_i}
-    <tr>
-      <td class="dia">{WEEKDAYS[day_i]}</td>
-      {#each day as working_day}
-        {#if working_day.special}
-          <td class="special">{working_day.date} <br>({WEEKDAYS[myWeekDay(working_day.timestamp)]})</td>
-        {:else}
-          <td>{working_day.date}</td>
-        {/if}
-      {/each}
-      <td>
-        {#if formatted_holidays[day_i].length}
-          {#each formatted_holidays[day_i] as holiday}
-            <div>{formatDate(holiday.timestamp, false)}</div>
+      <div class="flex items-center">
+        <Checkbox bind:status={use_saturdays}>
+          Usar Sábados
+        </Checkbox>
+      </div>
+
+      <button class="bg-green-200 hover:bg-green-400 rounded-md" on:click={() => show_holidays = true}>Feriados</button>
+    </div>
+
+    <!-- <button class:active="{days_per_week == 5}" on:click={() => days_per_week = 5}>
+      Segunda a Sexta (5 dias letivos)
+    </button>
+    <button class:active="{days_per_week == 6}" on:click={() => days_per_week = 6}>
+      Segunda a Sábado (6 dias letivos)
+    </button> -->
+
+    {#if show_holidays}
+      <div class="fixed z-10 inset-0 overflow-y-auto">
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+
+          <!-- Background overlay -->
+          <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+
+          <!-- This element is to trick the browser into centering the modal contents. -->
+          <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+          <!-- modal -->
+          <div class="inline-block align-bottom bg-gray-100 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg lg:max-w-2xl sm:w-full p-3">
+            <div class="text-lg font-bold mb-2">
+              Feriados
+              <span class="float-right text-sm border border-gray-800 px-1.5 pb-1 hover:text-white hover:bg-gray-800 cursor-pointer" on:click={() => show_holidays = false}>&times;</span>
+            </div>
+            <div class="grid grid-cols-2 lg:grid-cols-3 gap-2">
+              {#each fixed_holidays_dates as holiday}
+                {#if holidays[holiday]}
+                  <div class="p-1 bg-blue-100 rounded-md h-16">
+                    <div>
+                      {holiday}
+                      <div class="float-right bg-white rounded-xl inline-block px-2 py-1 text-xs cursor-pointer hover:shadow-lg" on:click={() => confirm("Quer mesmo remover o feriado?") ? removeHoliday(holiday) : ''}>remover</div>
+                    </div>
+                    <div class="text-xs">{holidays[holiday]}</div>
+                  </div>
+                {:else}
+                  <div class="p-1 bg-blue-300 rounded-md h-16">
+                    <div>{holiday}</div>
+                    <div class="text-xs">{NATIONAL_HOLIDAYS[holiday]}</div>
+                  </div>
+                {/if}
+              {/each}
+            </div>
+            <div class="font-bold my-2">
+              Feriados móveis
+            </div>
+            <div class="grid grid-cols-2 lg:grid-cols-3 gap-2">
+              {#each moving_holidays_dates as holiday}
+                <div class="p-1 bg-blue-100 rounded-md h-16">
+                  <div>
+                    {holiday}
+                    <div class="float-right bg-white rounded-xl inline-block px-2 py-1 text-xs cursor-pointer hover:shadow-lg" on:click={() => confirm("Quer mesmo remover o feriado?") ? removeHoliday(holiday) : ''}>remover</div>
+                  </div>
+                  <div class="text-xs">{holidays[holiday]}</div>
+                </div>
+              {/each}
+            </div>
+            <div class="font-bold my-2">
+              Adicionar feriado
+            </div>
+            <div class="grid grid-cols-2 gap-2 p-2 border rounded-md shadow-lg bg-white">
+              <div class="h-20">
+                <label for="holiday_date" class="text-md block">Dia:</label>
+                <input class="border p-2 w-full rounded-md" id="holiday_date" bind:value={start_date} type="date">
+              </div>
+              <div class="p-2 flex items-center justify-center">
+                <Checkbox color_on="blue-500">
+                  Feriado fixo
+                </Checkbox>
+              </div>
+              <div>
+                <label for="holiday_desc" class="text-md block">Descrição:</label>
+                <input class="border p-2 w-full rounded-md" id="holiday_desc" type="text">
+              </div>
+              <div class="align-center">
+                <label class="text-md block">&nbsp;</label>
+                <div class="block w-full flex justify-center">
+                  <button class="border p-2 px-12 bg-blue-200 rounded-md">Salvar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    {/if}
+
+    <div class="overflow-x-auto mt-4">
+
+      <table class="min-w-full border-2 border-green-900">
+        <tr>
+          <th class="border-2 border-green-900 p-1">Semana</th>
+          {#each range(number_of_weeks) as week}
+            <th class="border-2 border-green-900 p-1">{week + 1}</th>
           {/each}
-        {/if}
-      </td>
-    </tr>
-  {/each}
-</table>
-Último dia letivo: {formatDate(end_timestamp)}
+          <th class="border-2 border-green-900 p-1">Feriados</th>
+        </tr>
+        {#each formatted_days as day, day_i}
+          <tr>
+            <td class="border-2 border-green-900 p-1">{WEEKDAYS[day_i]}</td>
+            {#each day as working_day}
+              {#if working_day.special}
+                <td class="border-2 border-green-900 p-1 text-red-900 font-bold">{working_day.date} <br>({WEEKDAYS[myWeekDay(working_day.timestamp)]})</td>
+              {:else}
+                <td class="border-2 border-green-900 p-1">{working_day.date}</td>
+              {/if}
+            {/each}
+            <td>
+              {#if formatted_holidays[day_i].length}
+                {#each formatted_holidays[day_i] as holiday}
+                  <div>{formatDate(holiday.timestamp, false)}</div>
+                {/each}
+              {/if}
+            </td>
+          </tr>
+        {/each}
+      </table>
+    </div>
+    <div class="text-lg">
+      Último dia letivo: <span class="font-bold">{formatDate(end_timestamp)}</span>
+    </div>
+  </main>
+</div>
---
+++ src/Checkbox.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let status = false
+  export let color_on = 'green-500'
+  export let color_off = 'gray-500'
+  export let disabled
+</script>
+
+
+<label class="relative pr-2">
+  <input type="checkbox" class="hidden" disabled="{disabled}" bind:checked="{status}" />
+
+  <div class="absolute bg-gray-400 opacity-50 w-full h-full rounded-full z-10" class:hidden={!disabled}></div>
+
+  <div class="flex cursor-pointer">
+    <div class="h-6 w-14 flex items-center relative mr-2">
+      <div class="bg-gray-300 rounded-full w-14 h-6">
+      </div>
+      <div
+        class="w-5 h-5 {status ? `bg-${color_on}` : `bg-${color_off}`} rounded-full absolute"
+        class:left-1={status}
+        class:right-1={!status}
+      >
+      </div>
+    </div>
+
+    <slot></slot>
+  </div>
+</label>
