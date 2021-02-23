dc0fc2d34b51d6739aa4ac0ca71393992d13b12d
Ranieri Valença <ranierivalenca@gmail.com>
2021-02-22 23:11:08 -0300

    Calendar component now is binded to workingDays object and bounded by first and last days

--- src/Calendar.svelte
+++ src/Calendar.svelte
@@ -8,88 +8,76 @@
 
   const range = utils.range
 
+  let months = []
 
   $:(() => {
     update()
-    updateCalendar()
-  })(working_days.start_timestamp)
-
-  let month, year
-  let next_month, next_year
+    // updateCalendar()
+  })(working_days.start_timestamp, working_days.end_timestamp)
 
   let update = () => {
+    months = []
+    let last_month = +(new Date(working_days.end_timestamp).getMonth()) + 1
+    last_month = +last_month < 10 ? `0${+last_month}` : last_month
+    let last_year = new Date(working_days.end_timestamp).getFullYear()
+
+    let month = 0
+    let year = 0
     month = +(new Date(working_days.start_timestamp).getMonth()) + 1;
+    month = +month < 10 ? `0${+month}` : month
     year = new Date(working_days.start_timestamp).getFullYear()
+    while (+`${year}${month}` <= +`${last_year}${last_month}`) {
+      months.push([month, year])
+      month = +month == 12 ? "01" : +month + 1
+      month = +month < 10 ? `0${+month}` : month
+      year = +month == 1 ? +year + 1 : year
+    }
+    console.log(months)
   }
 
-  let updateNext = () => {
-    next_month = +month == 12 ? "01" : +month + 1
-    next_year = +next_month == 1 ? +year + 1 : year
-  }
-
-  let next = () => {
-    month = +month == 12 ? "01" : +month + 1
-    year = +month == 1 ? +year + 1 : year
-    updateNext()
-    updateCalendar()
-  }
-  let prev = () => {
-    month = +month == 1 ? "12" : +month - 1
-    year = +month == 12 ? +year - 1 : year
-    updateNext()
-    updateCalendar()
-  }
-
-  let calendar = []
-
-  let updateCalendar = () => {
-    updateNext()
+  let calendar = (month, year, first = false) => {
+    let next_month = +month == 12 ? "01" : +month + 1
+    let next_year = +next_month == 1 ? +year + 1 : year
 
     let initial_ts = Date.parse(`${year}-${month}-01 12:00`)
     let end_ts = Date.parse(`${next_year}-${next_month}-01 12:00`)
     let count_days = (end_ts - initial_ts) / DAY
+
     let days = Array(count_days).fill(0).map((d, i) => new Date(initial_ts + i * DAY))
 
-    calendar = [
+    return [
       ...range(days[0].getDay()).fill(null).map((d, i) => {
-        return new Date(days[0].getTime() - (days[0].getDay() - i) * DAY)
+        return first ? new Date(days[0].getTime() - (days[0].getDay() - i) * DAY) : null
       }),
       ...days,
-      ...range(6 - days[days.length - 1].getDay()).fill(null).map((d, i) => {
-        return new Date(days[days.length - 1].getTime() + (i + 1) * DAY)
-      })
     ]
   }
-  update()
-  updateCalendar()
 </script>
 
-<div class="grid grid-cols-3 mb-2 text-center">
-  <a class="font-bold" href="#" on:click|preventDefault={prev}>Anterior</a>
-  <span>{month} / {year}</span>
-  <a class="font-bold" href="#" on:click|preventDefault={next}>Próximo</a>
-</div>
-<div class="grid grid-cols-7 text-center gap-2">
-  {#each WEEKDAYS as day}
-    <div class="font-bold">{day}</div>
-  {/each}
-  {#each calendar as day}
-    <div>
-      <div
-        class="h-full rounded-lg"
-        class:bg-green-300={
-          !!day && !!working_days && !!working_days.getDay(day.getTime()) && +day.getMonth() + 1 == +month
-        }
-        class:text-gray-400={
-          !!day &&
-          !!working_days &&
-          (!!!working_days.getDay(day.getTime()) || +day.getMonth() + 1 != +month) &&
-          !!!working_days.holidays.isHoliday(day.getTime())
-        }
-        class:bg-blue-500|text-white={!!day && !!working_days && !!working_days.holidays.isHoliday(day.getTime())}
-      >
-        {day ? day.getDate() : ''}
+{#each months as [month, year], i}
+  <h2 class="border-t border-gray-300 pt-4 pb-4 text-lg font-bold text-center mt-2">{month} / {year}</h2>
+  <div class="grid grid-cols-7 text-center gap-2">
+    {#each WEEKDAYS as day}
+      <div class="font-bold">{day}</div>
+    {/each}
+    {#each calendar(month, year, i == 0) as day}
+      <div>
+        <div
+          class="h-full rounded-lg"
+          class:bg-green-300={
+            !!day && !!working_days && !!working_days.getDay(day.getTime()) && +day.getMonth() + 1 == +month
+          }
+          class:text-gray-400={
+            !!day &&
+            !!working_days &&
+            (!!!working_days.getDay(day.getTime()) || +day.getMonth() + 1 != +month) &&
+            !!!working_days.holidays.isHoliday(day.getTime())
+          }
+          class:bg-blue-500|text-white={!!day && !!working_days && !!working_days.holidays.isHoliday(day.getTime())}
+        >
+          {day ? day.getDate() : ''}
+        </div>
       </div>
-    </div>
-  {/each}
-</div>
+    {/each}
+  </div>
+{/each}
